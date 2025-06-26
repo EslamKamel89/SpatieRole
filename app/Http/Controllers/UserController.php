4e4db8e645e62ef9feefd8e36b1bb847ddc11923
EslamKamel89 <elsamkamelforex@gmail.com>
2025-06-26 21:00:35 +0300

    Refactor UserController to use UserResource for user data transformation and enhance Index.vue to display user details in a table format.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -11,7 +12,8 @@ class UserController extends Controller {
      */
     public function index() {
         return inertia('admin/users/Index', [
-            'users' => User::all(),
+            'users' => UserResource::collection(User::all()),
+            // 'users' => User::all(),
         ]);
     }
 
--- resources/js/pages/admin/users/Index.vue
+++ resources/js/pages/admin/users/Index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { User } from '@/types';
 const props = defineProps<{
@@ -8,7 +9,27 @@ const props = defineProps<{
 <template>
     <AppLayout>
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-            <pre>{{ users }}</pre>
+            <Table>
+                <TableCaption>A list of your recent invoices.</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead class="w-[100px]"> Name </TableHead>
+                        <TableHead>Email</TableHead>
+                        <TableHead class="text-right"> Created At </TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow v-for="user in users" :key="user.id">
+                        <TableCell class="font-medium">
+                            {{ user.name }}
+                        </TableCell>
+                        <TableCell>{{ user.email }}</TableCell>
+                        <TableCell class="text-right">
+                            {{ user.created_at }}
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
         </div>
     </AppLayout>
 </template>
