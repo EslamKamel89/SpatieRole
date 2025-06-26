1657d8358c14278f72a378eb6aedaa8fcdd948de
EslamKamel89 <elsamkamelforex@gmail.com>
2025-06-26 21:09:35 +0300

    Update AppLogo and AppSidebar components; modify Index.vue to enhance user table with action buttons and improve layout

--- resources/js/pages/admin/users/Index.vue
+++ resources/js/pages/admin/users/Index.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import Button from '@/components/ui/button/Button.vue';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { User } from '@/types';
+import { Pen, Plus, Trash2 } from 'lucide-vue-next';
 const props = defineProps<{
     users: User[];
 }>();
@@ -9,13 +11,22 @@ const props = defineProps<{
 <template>
     <AppLayout>
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+            <div class="flex w-full justify-end">
+                <Button type="button">
+                    <Plus />
+                    <span>User</span>
+                </Button>
+            </div>
             <Table>
-                <TableCaption>A list of your recent invoices.</TableCaption>
+                <!--
+                    <TableCaption>A list of your recent invoices.</TableCaption>
+                -->
                 <TableHeader>
                     <TableRow>
                         <TableHead class="w-[100px]"> Name </TableHead>
                         <TableHead>Email</TableHead>
                         <TableHead class="text-right"> Created At </TableHead>
+                        <TableHead class="text-right"> Actions </TableHead>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
@@ -24,8 +35,12 @@ const props = defineProps<{
                             {{ user.name }}
                         </TableCell>
                         <TableCell>{{ user.email }}</TableCell>
-                        <TableCell class="text-right">
-                            {{ user.created_at }}
+                        <TableCell> {{ user.created_at }}</TableCell>
+                        <TableCell class="">
+                            <div class="flex w-full items-center justify-end space-x-2">
+                                <Button type="button" variant="destructive" size="sm"><Trash2 /></Button>
+                                <Button type="button" variant="secondary" size="sm"><Pen /></Button>
+                            </div>
                         </TableCell>
                     </TableRow>
                 </TableBody>
