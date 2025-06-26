e08d7edc1afa67d752d48dca9fff7c2a732b8341
EslamKamel89 <elsamkamelforex@gmail.com>
2025-06-26 20:43:10 +0300

    Add UserController methods and create Users index page

---
+++ resources/js/pages/admin/users/Index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+</script>
+<template>
+    <AppLayout>
+        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">Users index</div>
+    </AppLayout>
+</template>
